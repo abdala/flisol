61507e45b75673fe02440e26701c5cb9bd85bb5a
Abdala Cerqueira <abdala.cerqueira@gmail.com>
2014-04-26 10:15:01 -0300

    Exemplos de utilização do shellwrap

--- php-em-todos-os-lugares/public/shellwrap/index.php
+++ php-em-todos-os-lugares/public/shellwrap/index.php
@@ -1,2 +1,33 @@
 <?php
-echo "oi";
+require_once '../../vendor/autoload.php';
+
+use MrRio\ShellWrap as sh;
+/*
+
+*/
+echo sh::ls();
+
+echo sh::grep('html', sh::curl('http://google.com', array(
+    'location' => true
+)));
+
+
+sh::touch('file.html');
+
+sh::rm('file.html');
+
+
+sh::git('checkout', 'master');
+sh::git('reset', array('hard' => true), 'HEAD');
+sh::git('reset', '--hard', 'HEAD');
+sh::git(array('reset', '--hard', 'HEAD'));
+
+// date --date '2012-10-10 10:00:00'
+echo sh::date(array(
+    'date' => '2012-10-10 10:00:00'
+));
+
+// date -d '2012-10-10 10:00:00'
+echo sh::date(array(
+    'd' => '2012-10-10 10:00:00'
+));
